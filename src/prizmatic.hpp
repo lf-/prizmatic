4d9d03b34bd9858e640efdbc7a65edc3e5057b85
lf <software@lfcode.ca>
2019-02-12 11:57:51 -0600

    Add some debug

--- src/prizmatic.hpp
+++ src/prizmatic.hpp
@@ -3,12 +3,20 @@
 #include <stdint.h>
 #include <initializer_list>
 
+#ifdef DEBUG_PRIZMATIC
+#define DBG(s) Serial.println(s)
+#define DBGN(s) Serial.print(s)
+#else
+#define DBG(s)
+#define DBGN(s)
+#endif
+
 struct Step {
     long left;
     long right;
 };
 
-class PRIZMatic : PRIZM {
+class PRIZMatic : public PRIZM {
    public:
     void drive_steps(long speed, std::initializer_list<Step> steps);
     void wait_for_start_button();
@@ -26,8 +34,10 @@ void PRIZMatic::drive_steps(long speed, std::initializer_list<Step> steps) {
 }
 
 void PRIZMatic::wait_for_start_button() {
+    DBG("waiting for start button....");
     while (!this->readStartButton()) {
     }
+    DBG("started!");
 }
 
 template <typename T>
@@ -45,7 +55,7 @@ int8_t PRIZMatic::read_rc(uint8_t pin) {
     if (pulseWidth == 0) {
         return 0;
     }
-    return (clamp<signed long>((pulseWidth - 1200), 0, 511) - 256) / 2;
+    return (clamp<signed long>((pulseWidth - 1200), 0, 510) - 255) / 2;
 }
 
 template <typename T>
@@ -64,15 +74,18 @@ void PRIZMatic::begin_rc_control(long speed) {
     // change these if signalling switch doesn't work
     const int8_t switch_on = 50;
     const int8_t switch_off = -50;
-    const uint8_t signal_switch_pin = 3;  // Port D3
-    const uint8_t fwd_rev_pin = 4;        // Port D4
-    const uint8_t turn_pin = 5;           // Port D5
+    const uint8_t signal_switch_pin = 17;  // Port A1
+    const uint8_t turn_pin = 15;           // Port A2
+    const uint8_t fwd_rev_pin = 16;        // Port A3
 
     bool done = 0;
     unsigned long done_timer = 0;
 
     while (!done) {
-        auto sw = read_rc(signal_switch_pin);
+        delay(400);
+        auto sw = -read_rc(signal_switch_pin);
+        // DBG("switch:");
+        // DBG(sw);
         if (sw > switch_on) {
             if (!done_timer) {
                 // start timer
@@ -99,18 +112,26 @@ void PRIZMatic::begin_rc_control(long speed) {
             }
         }
 
-        auto throttle = read_rc(fwd_rev_pin);
+        auto throttle = -read_rc(fwd_rev_pin);
         auto turn = read_rc(turn_pin);
+        DBGN("throttle: ");
+        DBGN(throttle);
+        DBGN("turn: ");
+        DBG(turn);
         if (turn > switch_on || turn < switch_off) {
             auto turn_dir = sgn<int8_t>(turn);
             this->setMotorSpeeds(speed * turn_dir, -speed * turn_dir);
             // we want to ignore throttle inputs when turning
+            DBGN("turn dir:");
+            DBG(turn_dir);
             continue;
         }
 
         if (throttle > switch_on || throttle < switch_off) {
             auto motor_speed = speed * sgn<int8_t>(throttle);
             this->setMotorSpeeds(motor_speed, motor_speed);
+            DBGN("Set motor speed:");
+            DBG(motor_speed);
             continue;
         }
 
