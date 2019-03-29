712e91aec203d75caeb82a8a58655c708dd21bce
lf <software@lfcode.ca>
2019-03-29 13:06:26 -0600

    Redo initial steps

--- src/main.cpp
+++ src/main.cpp
@@ -19,21 +19,20 @@ void setup() {
     prizm.setServoPosition(1, SERVO_COLLECT);
 
     prizm.drive_steps(300, {
-                               {-9886, -9878},
+                               {-10734, -10736},
                            });
-
-    prizm.drive_steps(75, {{-406, -404}});
-    prizm.drive_steps(30, {
-                              {-35, 37},
-                              {-823, -827},
-                              {55, -50},
-                              {-152, -148},
-                              {9, -1},
-                              {-48, -46},
+    prizm.drive_steps(75, {
+                              {-620, -618},
                           });
-
-    prizm.setServoSpeed(1, 25);
+    prizm.drive_steps(30, {
+                              {-401, -427},
+                          });
+    // prizm.begin_rc_control(300, true);
+    // prizm.begin_rc_control(75, true);
+    // prizm.begin_rc_control(30, true);
+    prizm.setServoSpeed(1, 10);
     prizm.setServoPosition(1, SERVO_DRIVE);
+    delay(1000);
 
     prizm.drive_steps(300,
                       {{1267, 1254}, {-191, 193}, {3148, 3159}, {224, -219},
@@ -43,7 +42,7 @@ void setup() {
                        {6743, 6744}, {-580, 599}, {2308, 2311}, {-1267, 1266},
                        {2099, 2104}, {-568, 591}, {2511, 2522}, {-1207, 1222},
                        {406, 419},   {-825, 826}, {1677, 1684}, {-185, 187},
-                       {3984, 3981}, {-177, 172}, {4641, 4640}});
+                       {3984, 3981}, {-177, 172}, {4000, 4000}});
 
     prizm.setServoSpeed(1, 100);
     prizm.setServoPosition(1, SERVO_LAUNCH);
