b9933e3f897e473dfc0a240feee0ea17ef5ff803
lf- <lf-@users.noreply.github.com>
2019-05-24 22:45:59 -0600

    Turn on PrizmBegin again

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include "prizmatic.hpp"
 
-PRIZMatic prizm;
+PRIZMatic prizm{};
 
 const int SERVO_COLLECT = 39;
 const int SERVO_LAUNCH = 130;
@@ -9,8 +9,8 @@ const int SERVO_DRIVE = 70;
 void setup() {
     Serial.begin(9600);
     delay(200);
-    //prizm.dump_eeprom_steps();
-    //prizm.PrizmBegin();
+    // prizm.dump_eeprom_steps();
+    prizm.PrizmBegin();
     prizm.setMotorInvert(2, 1);
 
     prizm.begin_rc_control(300);
